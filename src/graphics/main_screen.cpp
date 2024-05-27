052397984edf0b0c3803d4595ab561dc9e3eef2a
VAlikV <alik.valiullin2002@yandex.ru>
2024-05-28 00:18:59 +0300

    added cursor and touch

--- src/graphics/main_screen.cpp
+++ src/graphics/main_screen.cpp
@@ -15,16 +15,21 @@ void drawMenu(uint8_t selected)
 {
     uint8_t add_const[2] = {0, 0};
     add_const[selected] = 5;
-    M5Dial.Display.clear();
+
+    //M5Dial.Display.clear();
+    M5Dial.Display.fillRect(0, M5Dial.Display.height()/2 - 27, M5Dial.Display.width(), 54, BLACK);
+
     M5Dial.Display.fillCircle(M5Dial.Display.width()*0.13, M5Dial.Display.height()/2, 20 + add_const[0], RED);
     //M5Dial.Display.drawString("A", M5Dial.Display.width()*0.13, M5Dial.Display.height()/2);
 
     M5Dial.Display.fillCircle(M5Dial.Display.width()*0.87, M5Dial.Display.height()/2, 20 + add_const[1], BLUE);
-    M5Dial.Display.display();
     //M5Dial.Display.drawString("B", M5Dial.Display.width()*0.8745, M5Dial.Display.height()/2);
+
+    drawCursor(selected);
 }
 
-void drawCursor()
+void drawCursor(uint8_t selected)
 {
-
+    float angle[2] = {3.1415, 0};
+    M5Dial.Display.fillCircle(M5Dial.Display.width()/2 + M5Dial.Display.width()*0.2*cos(angle[selected]), M5Dial.Display.height()/2, 5, WHITE);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@ bool update = true;
 long oldPosition = 0;
 long newPosition = 0;
 
+static constexpr const char* state_name[16] = {
+    "none", "touch", "touch_end", "touch_begin",
+    "___",  "hold",  "hold_end",  "hold_begin",
+    "___",  "flick", "flick_end", "flick_begin",
+    "___",  "drag",  "drag_end",  "drag_begin"};
+
+static m5::touch_state_t prev_state;
+
 void setup()
 {
     setupM5Dial();
@@ -13,7 +21,7 @@ void setup()
 
 void loop() 
 {
-    //M5Dial.update();
+    M5Dial.update();
     newPosition = M5Dial.Encoder.read();
 
     if (abs(newPosition - oldPosition) >= 4) {
@@ -31,5 +39,15 @@ void loop()
         M5Dial.Display.drawString(String(newPosition), M5Dial.Display.width()/2, M5Dial.Display.height()/2);
         oldPosition = newPosition;
     }
-    
+
+    auto t = M5Dial.Touch.getDetail();
+
+    if (prev_state != t.state) {
+        prev_state= t.state;
+        M5Dial.Display.fillRect(M5Dial.Display.width()/2-40, M5Dial.Display.height()/2-27, 80, 54, BLACK);
+        oldPosition = 0;
+        M5Dial.Encoder.write(0);
+        M5Dial.Display.drawString(String(oldPosition), M5Dial.Display.width()/2, M5Dial.Display.height()/2);
+    }
+
 }
